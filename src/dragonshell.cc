52ffcc02c36b08a4742c15c3552bdc8daac7c154
gandhi56 <gandhi21299@gmail.com>
2019-09-18 00:03:42 -0600

    trying to get process running in the background

--- src/dragonshell.cc
+++ src/dragonshell.cc
@@ -6,43 +6,38 @@
 #define NUM_ARGS		20
 
 #include <vector>
-#include <string>
 #include <cstring>
-#include <iostream>
+//#include <cstring>
 
 #include <unistd.h>	// system calls
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
 
-#ifdef beta
-	#include <dirent.h>	// for "ls" command, not required by assignment
-#endif
-
 void display_splash(){
-	std::cout<< "                                   ______________                                           					"<<std::endl;
-	std::cout<< "                             ,===:'.,            `-._                                       					"<<std::endl;
-	std::cout<< "                                  `:.`---.__         `-._                                   					"<<std::endl;
-	std::cout<< "                                    `:.     `--.         `.                                 					"<<std::endl;
-	std::cout<< "                                      \\.        `.         `.                               					"<<std::endl;
-	std::cout<< "                              (,,(,    \\.         `.   ____,-`.,                            					"<<std::endl;
-	std::cout<< "                           (,'     `/   \\.   ,--.___`.'                                     					"<<std::endl;
-	std::cout<< "                       ,  ,'  ,--.  `,   \\.;'         `                                     					"<<std::endl;
-	std::cout<< "                        `{D, {    \\  :    \\;                                                					"<<std::endl;
-	std::cout<< "                          V,,'    /  /    //                                                					"<<std::endl;
-	std::cout<< "                          j;;    /  ,' ,-//.    ,---.      ,                                					"<<std::endl;
-	std::cout<< "                          \\;'   /  ,' /  _  \\  /  _  \\   ,'/                                				"<<std::endl;
-	std::cout<< "                                \\   `'  / \\  `'  / \\  `.' /                                 				"<<std::endl;
-	std::cout<< "                                 `.___,'   `.__,'   `.__,'                                  					"<<std::endl;
-	std::cout<< "                                                                                            					"<<std::endl;
-	std::cout<< "__      __   _                    _         ___                           ___ _        _ _ 					"<<std::endl;
-	std::cout<< "\\ \\    / /__| |__ ___ _ __  ___  | |_ ___  |   \\ _ _ __ _ __ _ ___ _ _   / __| |_  ___| | | 				"<<std::endl;
-	std::cout<< " \\ \\/\\/ / -_) / _/ _ \\ '  \\/ -_) |  _/ _ \\ | |) | '_/ _` / _` / _ \\ ' \\  \\__ \\ ' \\/ -_) | | 		"<<std::endl;
-	std::cout<< "  \\_/\\_/\\___|_\\__\\___/_|_|_\\___|  \\__\\___/ |___/|_| \\__,_\\__, \\___/_||_| |___/_||_\\___|_|_|		"<<std::endl;
-	std::cout<< "                                                         |___/													"<<std::endl;
-	std::cout<< "																												"<<std::endl;
-	std::cout<< "																												"<<std::endl;
-	std::cout<< "																												"<<std::endl;
+	printf("                                   ______________                                           					\n");
+	printf("                             ,===:'.,            `-._                                       					\n");
+	printf("                                  `:.`---.__         `-._                                   					\n");
+	printf("                                    `:.     `--.         `.                                 					\n");
+	printf("                                      \\.        `.         `.                               					\n");
+	printf("                              (,,(,    \\.         `.   ____,-`.,                            					\n");
+	printf("                           (,'     `/   \\.   ,--.___`.'                                     					\n");
+	printf("                       ,  ,'  ,--.  `,   \\.;'         `                                     					\n");
+	printf("                        `{D, {    \\  :    \\;                                                					\n");
+	printf("                          V,,'    /  /    //                                                					\n");
+	printf("                          j;;    /  ,' ,-//.    ,---.      ,                                					\n");
+	printf("                          \\;'   /  ,' /  _  \\  /  _  \\   ,'/                                					\n");
+	printf("                                \\   `'  / \\  `'  / \\  `.' /                                 					\n");
+	printf("                                 `.___,'   `.__,'   `.__,'                                  					\n");
+	printf("                                                                                            					\n");
+	printf("__      __   _                    _         ___                           ___ _        _ _ 						\n");
+	printf("\\ \\    / /__| |__ ___ _ __  ___  | |_ ___  |   \\ _ _ __ _ __ _ ___ _ _   / __| |_  ___| | | 					\n");
+	printf(" \\ \\/\\/ / -_) / _/ _ \\ '  \\/ -_) |  _/ _ \\ | |) | '_/ _` / _` / _ \\ ' \\  \\__ \\ ' \\/ -_) | | 			\n");
+	printf("  \\_/\\_/\\___|_\\__\\___/_|_|_\\___|  \\__\\___/ |___/|_| \\__,_\\__, \\___/_||_| |___/_||_\\___|_|_|			\n");
+	printf("                                                         |___/													\n");
+	printf("																												\n");
+	printf("																												\n");
+	printf("																												\n");
 }
 
 
@@ -52,7 +47,6 @@ void display_splash(){
  * @param str - The string to tokenize
  * @param delim - The string containing delimiter character(s)
  * @return std::vector<std::string> - The list of tokenized strings. Can be empty
- */
 
 std::vector<std::string> tokenize(const std::string &str, const char *delim) {
 	char* cstr = new char[str.size() + 1];
@@ -69,6 +63,7 @@ std::vector<std::string> tokenize(const std::string &str, const char *delim) {
 
 	return tokens;
 }
+ */
 
 /**
  * @brief Tokenize a C string
@@ -90,7 +85,7 @@ int tokenize_c(char* str, const char* delim, char ** argv) {
 	return count;
 }
 
-void run_cmd(char* arg){
+void run_cmd(char* arg, std::vector<int>& backProc){
 	char* buff[NUM_ARGS];
 	int len = tokenize_c(arg, "\n", buff);
 	len = tokenize_c(arg, " ", buff);
@@ -98,9 +93,12 @@ void run_cmd(char* arg){
 	// expected nullptr by system calls
 	buff[len] = nullptr;
 
+	// print current working directory
 	if (strcmp(buff[0], "pwd") == 0){
 		printf("%s\n", get_current_dir_name());
 	}
+
+	// change directory
 	else if (strcmp(buff[0], "cd") == 0){
 		if (len >= 2){
 			if (chdir(buff[1]) == -1){
@@ -117,54 +115,67 @@ void run_cmd(char* arg){
 	else if (strcmp(buff[0], "a2path") == 0){
 		// TODO
 	}
+
+	// call _exit system call
 	else if (strcmp(buff[0], "exit") == 0){
 		printf("Need to ensure all child processes have been terminated. ");
 		printf("Besides, thanks for using the dragonshell.\n");
-		exit(EXIT_SUCCESS);
-	}
-	else if (len){
-		pid_t pid = fork();
-		#ifdef DEBUG
-			printf("%s %d\n", SHELL, pid);
-		#endif
-		if (pid < 0){
-			printf("%s Could not create child process.\n", SHELL);
-		}
-		else if (pid == 0){
-			execvp(buff[0], buff);
-			exit(0);
-		}
-		else{
-			wait(nullptr);
-		}
+		_exit(0);
 	}
 
+	// execute command
+	else if (len){
+		
+		pid_t pid = fork();
+		if (pid > 0){
+			if (buff[len-1] == "&"){
+				
+			}
+			else{
+				while (1){
+					int status;
+					int wpid = wait(&status);
+					if (wpid == pid){
+						break;
+					}
+					//waitpid(pid, &status, 0);
+				}
+			}
+		}
+		else if (pid == 0){
+			// child process
+			execvp(buff[0], buff);
+		}
+		else{
+			printf("%s Could not create child process.\n", SHELL);
+			return;
+		}
+	}
 }
 
 int main(int argc, char **argv) {
 	// print the string prompt without a newline, before beginning to read
 	// tokenize the input, run the command(s), and print the result
 	// do this in a loop
-	
-
 	char 	cmd[MAX_LEN];
 	char* 	jobs[MAX_JOBS];
+	std::vector<int> backProc;
 	
 	display_splash();
 	
 	while (1){
 		
 		#ifdef beta
-			std::cout << "dragonshell: " << get_current_dir_name() << " > ";
+			printf("dragonshell: %s > ", get_current_dir_name());
 		#else
-			std::cout << "dragonshell: > ";
+			printf("dragonshell: > ");
 		#endif
 
 		fgets(cmd, MAX_LEN, stdin);
 		tokenize_c(cmd, "\n", jobs);
 		int numJobs = tokenize_c(cmd, ";", jobs);
 		for (int i = 0; i < numJobs; ++i){
-			run_cmd(jobs[i]);
+			run_cmd(jobs[i], backProc);
 		}
 
 	}
