f936c41c3a22a54a12788fbd7a1a9231672da859
gandhi56 <gandhi21299@gmail.com>
2019-09-22 13:50:50 -0600

    refactored code

--- src/dragonshell.cc
+++ src/dragonshell.cc
@@ -45,31 +45,6 @@ void display_splash(){
 	printf("																												\n");
 }
 
-
-/**
- * @brief Tokenize a string 
- * 
- * @param str - The string to tokenize
- * @param delim - The string containing delimiter character(s)
- * @return std::vector<std::string> - The list of tokenized strings. Can be empty
-
-std::vector<std::string> tokenize(const std::string &str, const char *delim) {
-	char* cstr = new char[str.size() + 1];
-	std::strcpy(cstr, str.c_str());
-
-	char* tokenized_string = strtok(cstr, delim);
-
-	std::vector<std::string> tokens;
-	while (tokenized_string != NULL){
-		tokens.push_back(std::string(tokenized_string));
-		tokenized_string = strtok(NULL, delim);
-	}
-	delete[] cstr;
-
-	return tokens;
-}
- */
-
 /**
  * @brief Tokenize a C string
  *
@@ -90,6 +65,85 @@ int tokenize_c(char* str, const char* delim, char ** argv) {
 	return count;
 }
 
+void change_dir(char* buff[NUM_ARGS], int& buffLen){
+	if (buffLen < 2){
+		printf("%s expected argument to \"cd\"\n", SHELL);
+		return;
+	}
+	if (chdir(buff[1]) == -1){
+		printf("%s No such file or directory\n", SHELL);
+	}
+}
+
+void update_path(char* buff[NUM_ARGS], int& buffLen){
+	// TODO is this implemented correctly?
+	char* env = getenv("PATH");
+	char* pathArgs[NUM_ARGS];
+	buffLen = tokenize_c(buff[1], ":", pathArgs);
+	for (int i = 0; i < buffLen; ++i){
+		printf("pathArgs[%d] = %s\n", i, pathArgs[i]);
+	}
+	if (buffLen == 0){
+		printf("Please provide the argument to run the command.\n");
+		return;
+	}
+	printf("len = %d\n", buffLen);
+	if (strcmp(pathArgs[0], "$PATH") == 0 or pathArgs[0][0] == '$'){
+		// append
+		//strcat(env, ":");
+		//strcat(env, pathArgs[1]);
+		printf("0 - adding %s...\n", pathArgs[1]);
+		setenv("PATH", pathArgs[1], 0);
+	}
+	else{
+		// overwrite
+		printf("1 - adding %s...\n", pathArgs[0]);
+		setenv("PATH", pathArgs[0], 1);
+	}
+}
+
+void run_child_bg(char* buff[NUM_ARGS], int& buffLen){
+	printf("Putting job %d in the background.\n", getpid());
+	int cmdLen = 0;
+	for (int i = 0; i < buffLen; ++i){
+		cmdLen += strlen(buff[i]);
+	}
+
+	char proc[cmdLen];
+	int k =0 ;
+	for (int i = 0; i < buffLen; ++i){
+		strcpy(&proc[k], buff[i]);
+	}
+
+	//close(STDIN_FILENO);
+	//close(STDOUT_FILENO);
+	//close(STDERR_FILENO);
+
+	char* child[buffLen];
+	for (int i =0 ; i < buffLen-1; ++i)
+		child[i] = buff[i];
+	child[buffLen-1] = NULL;
+
+	int x = open("/dev/null", O_RDWR);
+	printf("%d\n", x);
+	dup(x);
+	dup(x);
+	execvp(child[0], child);
+	_exit(1);
+}
+
+void run_child(char* buff[NUM_ARGS], int& procLen, int& fileDcpt){
+	char* proc[procLen];
+	for (int i = 0; i < procLen; ++i){
+		proc[i] = buff[i];
+	}
+	proc[procLen] = NULL;
+	if (fileDcpt > 0){
+		dup2(fileDcpt, 1);	// redirect output to file
+	}
+	execvp(proc[0], proc);
+}
+
 void run_cmd(char* arg, std::vector<int>& backProc){
 	char* buff[NUM_ARGS];
 	int len = tokenize_c(arg, "\n", buff);
@@ -100,48 +154,14 @@ void run_cmd(char* arg, std::vector<int>& backProc){
 
 	// change directory
 	if (strcmp(buff[0], "cd") == 0){
-		if (len >= 2){
-			if (chdir(buff[1]) == -1){
-				printf("%s No such file or directory\n", SHELL);
-			}
-		}
-		else{
-			printf("%s expected argument to \"cd\"\n", SHELL);
-		}
+		change_dir(buff, len);
 	}
 	else if (strcmp(buff[0], "$PATH") == 0){
-		char* env = getenv("PATH");
-		printf("Current PATH: %s\n", env);
+		printf("Current PATH: %s\n", getenv("PATH"));
 	}
 	else if (strcmp(buff[0], "a2path") == 0){
-		// TODO is this implemented correctly?
-		char* env = getenv("PATH");
-		char* pathArgs[NUM_ARGS];
-		len = tokenize_c(buff[1], ":", pathArgs);
-		for (int i = 0; i < len; ++i){
-			printf("pathArgs[%d] = %s\n", i, pathArgs[i]);
-		}
-		if (len == 0){
-			printf("Please provide the argument to run the command.\n");
-			return;
-		}
-		printf("len = %d\n", len);
-		if (strcmp(pathArgs[0], "$PATH") == 0 or pathArgs[0][0] == '$'){
-			// append
-			//strcat(env, ":");
-			//strcat(env, pathArgs[1]);
-			printf("0 - adding %s...\n", pathArgs[1]);
-			setenv("PATH", pathArgs[1], 0);
-		}
-		else{
-			// overwrite
-			printf("1 - adding %s...\n", pathArgs[0]);
-			setenv("PATH", pathArgs[0], 1);
-		}
-
+		update_path(buff, len);
 	}
-
-	// call _exit system call
 	else if (strcmp(buff[0], "exit") == 0){
 		printf("Thanks for using the dragonshell.\n");
 		_exit(0);
@@ -177,48 +197,11 @@ void run_cmd(char* arg, std::vector<int>& backProc){
 			}
 
 			if (buff[len-1][0] == '&'){
-				printf("Putting job %d in the background.\n", getpid());
-				int cmdLen = 0;
-				for (int i = 0; i < len; ++i){
-					cmdLen += strlen(buff[i]);
-				}
-
-				char proc[cmdLen];
-				int k =0 ;
-				for (int i = 0; i < len; ++i){
-					strcpy(&proc[k], buff[i]);
-				}
-
-				//close(STDIN_FILENO);
-				//close(STDOUT_FILENO);
-				//close(STDERR_FILENO);
-
-				char* child[len];
-				for (int i =0 ; i < len-1; ++i)
-					child[i] = buff[i];
-				child[len-1] = NULL;
-
-				int x = open("/dev/null", O_RDWR);
-				printf("%d\n", x);
-				dup(x);
-				dup(x);
-				execvp(child[0], child);
-				_exit(1);
+				run_child_bg(buff, len);
 			}
 			else{
-				char* proc[procLen];
-				for (int i = 0; i < procLen; ++i){
-					proc[i] = buff[i];
-					//printf("proc[%d] = %s\n", i, proc[i]);
-				}
-				proc[procLen] = NULL;
-				if (fileDcpt > 0){
-					dup2(fileDcpt, 1);
-				}
-				execvp(proc[0], proc);
+				run_child(buff, procLen, fileDcpt);
 			}
-
-
 		}
 
 		else if (pid > 0){
